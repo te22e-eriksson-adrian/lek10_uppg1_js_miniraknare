ed496fd02b40fc83979f3f52acb6b2c3147d4ea0
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-12-14 15:03:34 +0100

    finish - uppg1 lek10 om miniraknare

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ function plus(){
     svar.value=tal1+tal2;
 }
 function minus(){
+    console.log("minus");
     const tal1 = parseInt(get_tal1.value);
     const tal2 = parseInt(get_tal2.value);
     svar.value=tal1-tal2;
 }
 function multiplikation(){
+    console.log("multiplikation");
     const tal1 = parseInt(get_tal1.value);
     const tal2 = parseInt(get_tal2.value);
     svar.value=tal1*tal2;
 }
 function division(){
+    console.log("division");
     const tal1 = parseInt(get_tal1.value);
     const tal2 = parseInt(get_tal2.value);
     svar.value=tal1/tal2;
